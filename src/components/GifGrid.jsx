2b9f85070ada6cccdef15281e7ac95f75a27fffc
Sebaa <sebaamdq96@gmail.com>
2024-03-05 23:55:00 -0300

    create custom hook

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,18 +1,9 @@
-import { useState, useEffect } from "react";
-import { getGifs } from "../helpers/getGifs";
 import { GifItem } from "./GifItem";
+import { useFetchGifs } from "../hooks/useFetchGifs";
 
 export const GifGrid = ({ category }) => {
-  const [images, setImages] = useState([]);
-
-  const getImages = async () => {
-    const newImages = await getGifs(category);
-    setImages(newImages);
-  };
-
-  useEffect(() => {
-    getImages();
-  }, []);
+  
+  const {images, isLoading} = useFetchGifs(category);
 
   return (
     <div>
